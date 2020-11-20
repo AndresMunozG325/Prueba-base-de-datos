63e0b9c3d1a9344b058abbd2a0b1776c37e98755
Andres <andresmunozgo@gmail.com>
2020-11-19 22:40:54 -0300

    se cargan las facturas por clientes- comienzo pruebas

--- prueba_bd.sql
+++ prueba_bd.sql
@@ -278,6 +278,376 @@ VALUES (
     3
 );
 
+-- 10 facturas
+-- 2 para el cliente 1, con 2 y 3 productos
+
+INSERT INTO facturas (
+    n_factura,
+    fecha_factura,
+    subtotal,
+    iva,
+    total,
+    id_cliente
+)
+VALUES (
+    1,
+    '18-10-2020',
+    36000,
+    6840,
+    42840,
+    1
+);
+
+INSERT INTO facturas (
+    n_factura,
+    fecha_factura,
+    subtotal,
+    iva,
+    total,
+    id_cliente
+)
+VALUES (
+    2,
+    '18-10-2020',
+    54000,
+    10260,
+    64260,
+    1
+);
+
+INSERT INTO listado_productos (
+
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    1,
+    1,
+    8,
+    18000,
+    2,
+    36000
+);
+
+INSERT INTO listado_productos (
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    2,
+    1,
+    8,
+    18000,
+    3,
+    54000
+);
+
+-- 3 para el cliente 2, con 3, 2 y 3 productos
+
+INSERT INTO facturas (
+    n_factura,
+    fecha_factura,
+    subtotal,
+    iva,
+    total,
+    id_cliente
+)
+VALUES (
+    3,
+    '17-10-2020',
+    48000,
+    9120,
+    57120,
+    2
+);
+
+INSERT INTO facturas (
+    n_factura,
+    fecha_factura,
+    subtotal,
+    iva,
+    total,
+    id_cliente
+)
+VALUES (
+    4,
+    '17-10-2020',
+    20000,
+    3800,
+    23800,
+    2
+);
+
+INSERT INTO facturas (
+    n_factura,
+    fecha_factura,
+    subtotal,
+    iva,
+    total,
+    id_cliente
+)
+VALUES (
+    5,
+    '16-10-2020',
+    30000,
+    5700,
+    35700,
+    2
+);
+
+INSERT INTO listado_productos (
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    3,
+    3,
+    3,
+    16000,
+    3,
+    48000
+);
+
+INSERT INTO listado_productos (
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    4,
+    4,
+    2,
+    10000,
+    2,
+    20000
+);
+
+INSERT INTO listado_productos (
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    5,
+    5,
+    2,
+    10000,
+    3,
+    30000
+);
+
+-- 1 para el cliente 3, con 1 producto
+
+INSERT INTO facturas (
+    n_factura,
+    fecha_factura,
+    subtotal,
+    iva,
+    total,
+    id_cliente
+)
+VALUES (
+    6,
+    '19-10-2020',
+    10000,
+    1900,
+    11900,
+    3
+);
+
+INSERT INTO listado_productos (
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    6,
+    6,
+    2,
+    10000,
+    1,
+    10000
+);
+
+-- 4 para el cliente 4, con 2, 3, 4 y 1 producto
+
+INSERT INTO facturas (
+    n_factura,
+    fecha_factura,
+    subtotal,
+    iva,
+    total,
+    id_cliente
+)
+VALUES (
+    7,
+    '19-10-2020',
+    20000,
+    3800,
+    23800,
+    4
+);
+INSERT INTO facturas (
+    n_factura,
+    fecha_factura,
+    subtotal,
+    iva,
+    total,
+    id_cliente
+)
+VALUES (
+    8,
+    '20-10-2020',
+    30000,
+    5700,
+    35700,
+    4
+);
+INSERT INTO facturas (
+    n_factura,
+    fecha_factura,
+    subtotal,
+    iva,
+    total,
+    id_cliente
+)
+VALUES (
+    9,
+    '19-10-2020',
+    40000,
+    7600,
+    47600,
+    4
+);
+INSERT INTO facturas (
+    n_factura,
+    fecha_factura,
+    subtotal,
+    iva,
+    total,
+    id_cliente
+)
+VALUES (
+    10,
+    '19-10-2020',
+    10000,
+    1900,
+    11900,
+    4
+);
+
+INSERT INTO listado_productos (
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    7,
+    7,
+    6,
+    10000,
+    2,
+    20000
+);
+
+INSERT INTO listado_productos (
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    8,
+    8,
+    6,
+    10000,
+    3,
+    30000
+);
+
+INSERT INTO listado_productos (
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    9,
+    9,
+    6,
+    10000,
+    2,
+    20000
+);
+
+INSERT INTO listado_productos (
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    10,
+    9,
+    2,
+    10000,
+    2,
+    20000
+);
+
+INSERT INTO listado_productos (
+    id_listado,
+    n_factura,
+    id_producto,
+    precio_unitario,
+    cantidad,
+    total
+)
+VALUES (
+    11,
+    10,
+    2,
+    10000,
+    1,
+    10000
+);
+
+
+
+
+
+
+
 
 
 
